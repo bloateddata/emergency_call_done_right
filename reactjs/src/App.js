9e5ecbd2deda9cc753395fc1d3f61ee8574c21ff
Peter <pk>
2024-11-26 16:49:26 +0100

    move react project to subfolder

---
+++ reactjs/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import {Tree} from "@d3/tree-component"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        HELLO WORLD
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
